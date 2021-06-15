8deaa0060254bf91f14a46047502aed9fb8527c4
Jaivrat Das <jaivratdas123456@gmail.com>
2021-06-15 12:14:10 +0530

    Updated Menubar

--- src/Components/Appbar.js
+++ src/Components/Appbar.js
@@ -1,11 +1,6 @@
-import { useState } from 'react';
 import { useHistory, useLocation, Link } from "react-router-dom";
-import { v4 as uuid } from 'uuid';
 
 import AppBar from "@material-ui/core/AppBar";
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -14,17 +9,16 @@ import InputBase from "@material-ui/core/InputBase";
 import Toolbar from "@material-ui/core/Toolbar";
 import { fade, makeStyles } from "@material-ui/core/styles";
 
-import { pink } from '@material-ui/core/colors';
+import { useCycle } from "framer-motion";
 
-import MenuIcon from "@material-ui/icons/Menu";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from '@material-ui/icons/Home';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import CloudIcon from '@material-ui/icons/Cloud';
-import MapIcon from '@material-ui/icons/Map';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InfoIcon from '@material-ui/icons/Info';
+
+import Menubar, { MenubarIcon } from './Menubar';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -79,10 +73,13 @@ const useStyles = makeStyles((theme) => ({
 	},
     dummyToolbar: theme.mixins.toolbar,
     active: {
-        background: '#64b5f6',
+        background: '#fff2',
+        opacity: '1 !important',
     },
-    cloudIcon: {
-        color: pink['A400'],
+    menuText: {
+        '& *': {
+            fontWeight: 500,
+        },
     },
 }));
 
@@ -93,88 +90,52 @@ const Appbar = ({ appbarBG, handleSubmit, refreshData }) => {
 
     const classes = useStyles();
 
+    const colors = ["#00ff95", "#ff007a", "#00e0ff"];
+
     const menuItems = [
 
-        [
-            {
-                text: 'Weather',
-                icon: <CloudIcon className={ classes.cloudIcon } />,
-                path: '/'
-            },
-            {
-                text: 'Map',
-                icon: <MapIcon color="secondary" />,
-                path: '/map'
-            }
-        ],
-        [
-            {
-                text: 'Weather BGs',
-                icon: <PhotoLibraryIcon color="secondary" />,
-                path: '/weather-backgrounds'
-            },
-            {
-                text: 'About',
-                icon: <InfoIcon color="secondary" />,
-                path: '/about'
-            }
-        ]
+        {
+            text: 'Weather',
+            icon: <CloudIcon style={{ color: colors[0] }}/>,
+            path: '/',
+            color:  colors[0]
+        },
+        {
+            text: 'View All BGs',
+            icon: <PhotoLibraryIcon style={{ color: colors[1] }}/>,
+            path: '/weather-backgrounds',
+            color:  colors[1],
+            classes:  classes.menuText
+        },
+        {
+            text: 'About',
+            icon: <InfoIcon style={{ color: colors[2] }}/>,
+            path: '/about',
+            color:  colors[2]
+        }
     ];
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [isOpen, toggleOpen] = useCycle(false, true);
 
-    const toggleDrawer = (isOpen) => setIsDrawerOpen(isOpen);
+    const menuList = menuItems.map(item => (
 
-    const menu =  (
-
-        <div className={ classes.list }>
-
-            {menuItems.map(list => (
-                
-                <div key={ uuid() }>
-                    <List>
-                        {list.map(item => (
-
-                            <ListItem
-                                button
-                                key={ item.text }
-                                onClick={() => {
-                                    history.push(item.path);
-                                    toggleDrawer(false);
-                                }}
-                                className={
-                                    location.pathname === item.path ? classes.active : null
-                                }
-                            >
-                                <ListItemIcon>{ item.icon }</ListItemIcon>
-                                <ListItemText primary={ item.text } />
-                            </ListItem>
-                        ))}
-                    </List>
-                    { list !== menuItems[menuItems.length - 1] && <Divider /> }
-                </div>
-            ))}
-            
-        </div>
-    );
-
-    const drawer = (
-
-        <SwipeableDrawer
-            anchor="left"
-            open={ isDrawerOpen }
-            onClose={ () => toggleDrawer(false) }
-            onOpen={ () => toggleDrawer(true) }
+        <ListItem
+            button
+            onClick={() => {
+                history.push(item.path);
+                toggleOpen();
+            }}
+            className={ location.pathname === item.path ? classes.active : null }
+            disabled={ location.pathname === item.path }
+            style={{ color: item.color }}
+            key={ item.text }
         >
-            <div className={ classes.drawerHeader }>
-                <IconButton onClick={ () => toggleDrawer(false) }>
-                    <ChevronLeftIcon />
-                </IconButton>
-            </div>
-            <Divider />
-            { menu }
-        </SwipeableDrawer>
-    );
+            <ListItemIcon>
+                { item.icon }
+            </ListItemIcon>
+            <ListItemText className={ item.classes } primary={ item.text } />
+        </ListItem>
+    ));
 
     document.addEventListener('scroll', () => {
 
@@ -185,63 +146,58 @@ const Appbar = ({ appbarBG, handleSubmit, refreshData }) => {
     return (
 
         <>
-            <AppBar
-                className={ classes.appbar }
-                elevation={ 0 }
-            >
-                <Toolbar>
+        <AppBar className={ classes.appbar } elevation={ 0 }>
+            <Toolbar>
 
-                    <IconButton
-                        onClick={ () => toggleDrawer(true) }
-                        edge="start"
-                        color="inherit"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    { drawer }
+                <IconButton onClick={() => toggleOpen()} edge="start">
+                    <MenubarIcon isOpen={ isOpen } />
+                </IconButton>
 
-                    { location.pathname === '/' && (
-                        <>
-                        <div className={ classes.search }>
-                            <div className={ classes.searchField }>
-                                <form onSubmit={ handleSubmit }>
-                                    <div className={ classes.searchIcon }>
-                                        <SearchIcon />
-                                    </div>
-                                    <InputBase
-                                        placeholder="Location…"
-                                        classes={{
-                                            root: classes.inputRoot,
-                                            input: classes.inputInput,
-                                        }}
-                                    />
-                                </form>
-                            </div>
+                <Menubar isOpen={ isOpen } toggleOpen={ toggleOpen }>
+                    { menuList }
+                </Menubar>
+
+                { location.pathname === '/' && (
+                    <>
+                    <div className={ classes.search }>
+                        <div className={ classes.searchField }>
+                            <form onSubmit={ handleSubmit }>
+                                <div className={ classes.searchIcon }>
+                                    <SearchIcon />
+                                </div>
+                                <InputBase
+                                    placeholder="Location…"
+                                    classes={{
+                                        root: classes.inputRoot,
+                                        input: classes.inputInput,
+                                    }}
+                                />
+                            </form>
                         </div>
-                        <IconButton
-                            onClick={ refreshData }
-                            edge="end"
-                            color="inherit"
-                        >
-                            <RefreshIcon />
+                    </div>
+                    <IconButton onClick={ refreshData }
+                        edge="end" color="inherit"
+                    >
+                        <RefreshIcon />
+                    </IconButton>
+                    </>
+                )}
+
+                { location.pathname !== '/' && (
+                    <Link to="/" className={ classes.iconBtn }>
+                        <IconButton color="inherit">
+                            <HomeIcon className={ classes.icon } />
                         </IconButton>
-                        </>
-                    )}
-                    { location.pathname !== '/' && (
-                        <Link to="/" className={ classes.iconBtn }>
-                            <IconButton color="inherit">
-                                <HomeIcon className={ classes.icon } />
-                            </IconButton>
-                        </Link>
-                    )}
-                
-                </Toolbar>
+                    </Link>
+                )}
+            
+            </Toolbar>
 
-            </AppBar>
+        </AppBar>
 
-            <div className={ classes.dummyToolbar } />
+        <div className={ classes.dummyToolbar } />
         </>
     );
-}
+};
  
 export default Appbar;
---
+++ src/Components/Menubar.js
@@ -0,0 +1,130 @@
+import { motion } from "framer-motion";
+
+const sidebarVariants = {
+
+    open: (height) => ({
+        clipPath: `circle(${ height * 2 + 200 }px at 0px 0px)`,
+        transition: {
+            type: "spring",
+            stiffness: 20,
+            restDelta: 2
+        }
+    }),
+    closed: {
+        clipPath: "circle(0px at 0px 0px)",
+        transition: {
+            delay: 0.3,
+            type: "spring",
+            stiffness: 400,
+            damping: 40
+        }
+    }
+};
+
+const navVariants = {
+
+    open: {
+        transition: { staggerChildren: 0.05, delayChildren: 0.1 }
+    },
+    closed: {
+        transition: { staggerChildren: 0.05, staggerDirection: -1 }
+    }
+};
+
+const itemVariants = {
+
+    open: {
+        y: 0,
+        opacity: 1,
+        transition: { y: { stiffness: 1000, velocity: -100 } }
+    },
+    closed: {
+        y: -50,
+        opacity: 0,
+        transition: { y: { stiffness: 1000 } }
+    }
+};
+
+const Path = (props) => (
+
+    <motion.path
+        strokeWidth="2"
+        stroke="#fff"
+        strokeLinecap="round"
+        { ...props }
+    />
+);
+  
+export const MenubarIcon = ({ isOpen }) => {
+
+    // const strokeColor = isOpen ? 'hsl(0, 0%, 18%)' : '#fff';
+    
+    return (
+
+        <motion.svg className="menubar-icon"
+            width="23" height="23" viewBox="0 0 20 20"
+            initial={ false }
+            animate={ isOpen ? "open" : "closed" }
+        >
+            <Path
+                variants={{
+                    closed: { d: "M 2 2.5 L 20 2.5" },
+                    open: { d: "M 3 16.5 L 17 2.5" }
+                }}
+                // strokeColor={ strokeColor }
+            />
+            <Path
+                d="M 2 9.423 L 20 9.423"
+                variants={{
+                    closed: { opacity: 1 },
+                    open: { opacity: 0 }
+                }}
+                transition={{ duration: 0.1 }}
+                // strokeColor={ strokeColor }
+            />
+            <Path
+                variants={{
+                    closed: { d: "M 2 16.346 L 20 16.346" },
+                    open: { d: "M 3 2.5 L 17 16.346" }
+                }}
+                // strokeColor={ strokeColor }
+            />
+        </motion.svg>
+    )
+};
+
+const Menubar = ({ isOpen, toggleOpen, children }) => {
+
+    return (
+
+        <>
+        <motion.div className="backdrop"
+            onClick={() => toggleOpen()}
+            style={ isOpen ? { display: 'block' } : { display: 'none' } }
+            animate={ isOpen ? { opacity: 1 } : { opacity: 0 } }
+        />
+        <motion.nav
+            variants={ sidebarVariants }
+            initial={ false }
+            animate={ isOpen ? "open" : "closed" }
+            custom={ window.innerHeight }
+        >
+            <motion.ul variants={ navVariants }>
+                { children.map(child => (
+
+                    <motion.li
+                        variants={ itemVariants }
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.95 }}
+                        key={ child.key }
+                    >
+                        { child }
+                    </motion.li>
+                ))}
+            </motion.ul>
+        </motion.nav>
+        </>
+    );
+};
+
+export default Menubar;
